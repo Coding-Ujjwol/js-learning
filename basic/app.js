b6b77cc1a1012809cf796ab413ade324607593cb
Coding-Ujjwol <uupreti30@gmail.com>
2026-03-30 11:20:54 -0700

    data types

---
+++ basic/app.js
@@ -0,0 +1,12 @@
+// const account = 14455
+// let email = "uupreti29@gmail.com"
+// var password = "9849522272"
+// accountCity =  "kathmandu"
+
+// console.table([account, email, password, accountCity])    
+
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello World"); we are using nodejs, not browser 
+ 
+      
